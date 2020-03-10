38c459b106308a607f51a77409d7875e673c104e
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-03-10 17:43:24 -0300

    services cambiado los props

--- components/service.js
+++ components/service.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 const Service = (props)  => {
+    const props ={
+        titulo1: "The Second Heading",
+        titulo2: "Is Pretty Cool Too.",
+        parrafo1: "Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fucse dapibus, tellus ac cursus commodo."
+        imagen2: ".\components\img\2.jpg",
+    } 
+
     const style = {
         width :"100%", 
         borderRadius: "100%"}
@@ -8,11 +15,11 @@ const Service = (props)  => {
         <>
         <div className="row mt-3 stylish" id="second_heading">
             <div className="col-4 mt-2">
-                <img src=".\components\img\2.jpg" style = {style} className="" alt="..." />
+                <img src= {props.imagen2} style = {style} className="" alt="..." />
             </div>
             <div className="col-8 text-center">
-                <h1 className="">The Second Heading <span className="text-secondary">Is Pretty Cool Too.</span></h1>
-                <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fucse dapibus, tellus ac cursus commodo.</p>
+                <h1 className=""> {props.titulo1} <span className="text-secondary">{props.titulo2}</span></h1>
+                <p className="lead">{props.parrafo1}</p>
             </div>
         </div>
         </>
